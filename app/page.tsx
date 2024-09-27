8e3608cf091d87d255cb6c5acc91e7abe9f6aa3a
Danvick <danvickmiller@live.com>
2024-09-27 15:03:52 +0300

    feat: added @biopassid/face-sdk

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,20 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { camera } from '@biopassid/face-sdk';
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
+  const { takePicture } = camera();
+  async function handleTakePicture() {
+    const resp = await takePicture({
+      element: document.querySelector("#elementId"),
+    })
+  }
+  
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -23,6 +31,7 @@ export default function App() {
 
   useEffect(() => {
     listTodos();
+    handleTakePicture();
   }, []);
 
   function createTodo() {
@@ -33,7 +42,8 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <div id="elementId"></div>
+      {/* <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -46,7 +56,7 @@ export default function App() {
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
